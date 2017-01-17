a65868b4a2f8e5af808b99510ace90babec90cd3
ranga <ranganatha@holidayiq.com>
2017-01-17 18:47:07 +0530

    Initial commit

---
+++ samObj/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  samObj
+//
+//  Created by Ranganatha G V on 17/01/2017.
+//  Copyright © 2017 Cricbuzz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ samObj/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  samObj
+//
+//  Created by Ranganatha G V on 17/01/2017.
+//  Copyright © 2017 Cricbuzz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
